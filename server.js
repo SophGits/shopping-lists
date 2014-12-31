f2ca9c4b650bdf5590ab50d8c75e67ae75033a46
SophGits <sophie.jiffy@gmail.com>
2014-12-31 18:14:12 +0000

    Add HTTP verbs to handle requests.

--- server.js
+++ server.js
@@ -6,4 +6,31 @@ var app = express();
 
 app.use('/', express.static('assets'));
 
+app.post('/lists', function(req, res){
+  var body = {
+    id: 3
+  };
+  res.json(body);
+});
+
+app.put('/lists/:id', function(req, res){
+  res.json({});
+});
+
+app.post('/lists/:id/items', function(req, res){
+  var body = {
+    id: 4
+  }
+  res.json(body);
+});
+
+app.put('/lists/:list_id/items/:id', function(req, res){
+  res.json({});
+});
+
+app.delete('/lists/:list_id/items/:id', function(req, res){
+  res.json({});
+});
+
+
 app.listen(8888, function(){});
