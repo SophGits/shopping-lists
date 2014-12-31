0bfeb527a63f2727ee0e0acb5fa1c3253497a014
SophGits <sophie.jiffy@gmail.com>
2014-12-31 17:41:33 +0000

    Move index, styles and script into assets directory and serve them as top-level files from there.

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static('.')); // tells it to use this static middleware, using the current directory ('.')
+app.use('/', express.static('assets'));
 
-app.listen(8888, function(){
-  console.log("Ready. Server running on 8888")
-});
+app.listen(8888, function(){});
